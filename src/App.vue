771828247c5a94e723526169409c8d7f172694d5
hassenNachar <hnachar@gmail.com>
2022-07-31 16:59:03 +0200

    feat: start

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Culpa, delectus?</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+};
+</script>
+
+<style lang="scss">
+@import "src/scss/theme";
+</style>
