fe9c5a010d3cfc00f0396da4d4a1dce8cc97724a
hassenNachar <hnachar@gmail.com>
2022-08-02 00:30:47 +0200

    feat: display html content

---
+++ src/components/DisplayCodeComponent.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="col">
+        <div id="currentHtml"></div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'DisplayCodeComponent',
+  props: {
+    idElement: {
+      type: String,
+      require: true,
+    },
+  },
+  data() {
+    return {
+      currentHtml: undefined,
+    };
+  },
+  mounted() {
+    this.currentHtml = document.getElementById(this.idElement);
+    document.getElementById('currentHtml').appendChild(this.currentHtml);
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
--- src/components/HeroComponent.vue
+++ src/components/HeroComponent.vue
@@ -1,37 +1,41 @@
 <template>
-  <section class="hero app-hr min-vh-100 d-flex align-items-center">
-    <div class="container">
-      <div class="row">
-        <div class="col-12 col-lg-6">
-          <h1 class="font-weight-bold">{{ $t('title') }}</h1>
-          <h2 class="mb-4 mb-lg-0 font-weight-normal">{{ $t('job') }}</h2>
-        </div>
-        <div class="col-12 col-lg-6">
-          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eveniet, nemo.</p>
-          <p>Lorem ipsum dolor.</p>
-          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
-          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eveniet, nemo.</p>
-          <p class="mb-0">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+  <section class="app-hr py-4">
+    <div class="hero min-vh-100 d-flex align-items-center">
+      <div class="container">
+        <div class="row">
+          <div class="col-12 col-lg-6">
+            <h1 class="hero__title font-weight-bold">{{ $t('title') }}</h1>
+            <h2 class="hero__description mb-4 mb-lg-0 font-weight-normal">{{ $t('job') }}</h2>
+          </div>
+          <div class="col-12 col-lg-6">
+            <p>{{ $t('user_description') }}</p>
+          </div>
         </div>
       </div>
     </div>
+    <DisplayCodeComponent :id-element="'code-hero-component'"/>
   </section>
 </template>
 
 <script>
+import DisplayCodeComponent from '@/components/DisplayCodeComponent.vue';
+
 export default {
   name: 'HeroComponent',
+  components: {
+    DisplayCodeComponent,
+  },
 };
 </script>
 
 <style lang="scss" scoped>
 .hero {
-  h1 {
+  &__title {
     font-size: 4rem;
   }
-  h2 {
+
+  &__description {
     font-size: 2.5rem;
   }
 }
-
 </style>
