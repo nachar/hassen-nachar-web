dc9101d093aaf8c2f1507122772196594129500c
hassenNachar <hnachar@gmail.com>
2025-09-02 17:58:25 +0200

    feat: plugin vuetify

--- src/main.js
+++ src/main.js
@@ -1,17 +1,10 @@
-// Vuetify
 import 'vuetify/styles';
+import '@mdi/font/css/materialdesignicons.css';
 
 import { createApp } from 'vue';
-import { createVuetify } from 'vuetify';
-import * as components from 'vuetify/components';
-import * as directives from 'vuetify/directives';
 
-// Components
+import vuetify from '@/plugins/vuetify';
+
 import App from './App.vue';
-
-const vuetify = createVuetify({
-  components,
-  directives,
-});
 
 createApp(App).use(vuetify).mount('#app');
