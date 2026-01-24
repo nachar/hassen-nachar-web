20fbad5299ba15dfaa0d75c2c09c076176ac3db9
hassennachar <hnachar@gmail.com>
2026-01-24 16:30:31 +0100

    feat: i18 languages

--- src/components/ChatSection.vue
+++ src/components/ChatSection.vue
@@ -1,5 +1,3 @@
-<!-- TODO: Me faltan las traducciones a otros idiomas -->
-<!-- TODO: Highlight section -->
 <script setup>
 import { nextTick, ref, watch } from 'vue';
 import { useI18n } from 'vue-i18n';
